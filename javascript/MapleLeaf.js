e4e48cce269108739d3f43896e96dda386598fc4
jatcar95 <jacobtcarlson@gmail.com>
2014-11-08 16:48:56 -0800

    Lists text in bookmarks as links

--- javascript/MapleLeaf.js
+++ javascript/MapleLeaf.js
@@ -1,7 +1,19 @@
 "use strict";
-
+var userLeaf;
 window.onload = function() {
 	var bookmarks = ["website1", "website2"];
-	var userLeaf = new leaf(bookmarks, "Jake");
+	userLeaf = new leaf(bookmarks, "Jake");
 	document.getElementById("hello").innerHTML = "Hello " + userLeaf.username;
+	loadBookmarks();
+}
+
+function loadBookmarks() {
+	for (var i = 0; i < userLeaf.bookmarks.length; i++) {
+		var newBookmark = document.createElement("li");
+		var bookmarkLink = document.createElement("a");
+		bookmarkLink.setAttribute("href", "https://www.google.com");
+		bookmarkLink.innerHTML = userLeaf.bookmarks[i];
+		newBookmark.appendChild(bookmarkLink);
+		document.getElementById("bookmarks").appendChild(newBookmark);
+	}
 }
